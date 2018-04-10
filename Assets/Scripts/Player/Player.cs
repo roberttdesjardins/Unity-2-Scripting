6e82c84a4ac57540c56c06bb786b7ea548d5646c
Robert Desjardins <roberttdesjardins@gmail.com>
2018-04-10 13:24:41 -0700

    commit stuff

---
+++ Assets/Scripts/Player/Player.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public int health = 3;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void collidedWithEnemy(Enemy enemy) {
+		if (health <= 0) {
+			// TODO
+		}
+	}
+
+	// Triggers when two rigidbodies with colliders touch
+	void OnCollisionEnter(Collision col) {
+		Enemy enemy = col.collider.gameObject.GetComponent<Enemy> ();
+		collidedWithEnemy (enemy);
+	}
+}
